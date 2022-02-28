76f9eca537e92adb89b86cf36d7495315a339fe3
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-28 06:45:38 +0530

    Update logomaker.py

--- LovelyRobot/modules/logomaker.py
+++ LovelyRobot/modules/logomaker.py
@@ -40,7 +40,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Lovely_Ro_bot, {e}')
+   await event.reply(f'Error Report @Nakiri_Ayame_PROBOT, {e}')
 
 
 
@@ -80,7 +80,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Lovely_ro_bot, {e}')
+   await event.reply(f'Error Report @Nakiri_Ayame_PROBOT, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
